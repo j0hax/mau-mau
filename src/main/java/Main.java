a0e6fe3434f3a6a3f8d2bdd6f465e519d9f4b73a
Cedric Fauth <cidstudiosinfo@gmail.com>
2019-06-19 10:13:04 +0200

    Hello World und gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String hello = "Hello World";
+        GsonBuilder b = new GsonBuilder();
+        Gson gson = b.create();
+        String convert = gson.toJson(hello);
+
+        System.out.println(gson.fromJson(convert, String.class));
     }
 }
