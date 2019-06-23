e630958da7b24b15c2dd31f424611414d1dd6383
Kevin Kiessling <kiessling.kevin95@gmail.com>
2019-06-23 20:10:48 +0200

    Added Fxml controller class for the Login Fxml File to add functionality to the connect Button/ textfields

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,16 @@
+package client;
+/*
+* Basic Client Class. Gets implemented later
+* */
+public class Client {
+    private String name;
+    private int port;
+    private String serverIP;
+
+    public Client(String name, int port,String serverIP){
+        this.name=name;
+        this.port=port;
+        this.serverIP = serverIP;
+    }
+    // Gets implemented Later
+}
