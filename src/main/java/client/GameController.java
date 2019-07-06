7cb49bda5225534905658fe23dbba641986ffc5c
Kevin Kiessling <kiessling.kevin95@gmail.com>
2019-07-06 18:04:46 +0200

    added Imageview Buttons for the wish-selection Images are just placeholder for now, they can be changed easily via scene builder

--- src/main/java/client/GameController.java
+++ src/main/java/client/GameController.java
@@ -1,6 +1,7 @@
 package client;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import util.cards.Card;
@@ -13,10 +14,41 @@ public class GameController {
     public HBox hBox;
 
     @FXML
+    public ImageView spadesView;
+    @FXML
+    public ImageView heartsView;
+    @FXML
+    public ImageView clubsView;
+    @FXML
+    public ImageView diamondsView;
+
+
+    @FXML
     public void initialize() {
 
         Card c = new Card(CardSuite.CLUBS, CardRank.KING);
         hBox.getChildren().add(new ImageView(c.getImage()));
+
+    }
+
+    @FXML
+    public void spadesClicked(){
+        System.out.println("You've wished spades");
+
+    }
+    @FXML
+    public void heartsClicked(){
+        System.out.println("You've wished hearts");
+
+    }
+    @FXML
+    public void clubsClicked(){
+        System.out.println("You've wished clubs");
+
+    }
+    @FXML
+    public void diamondsClicked(){
+        System.out.println("You've wished diamonds");
 
     }
 
