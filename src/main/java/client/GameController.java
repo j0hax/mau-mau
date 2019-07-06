f34d79b81c463d847c6acaeb03ec1a6db3426f51
johannes.arnold <johannes.arnold@stud.uni-hannover.de>
2019-07-06 21:44:56 +0200

    Allow setting of the hand for the player

--- src/main/java/client/GameController.java
+++ src/main/java/client/GameController.java
@@ -1,13 +1,15 @@
 package client;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import util.cards.Card;
 import util.cards.CardRank;
 import util.cards.CardSuite;
+import util.protocol.DataPacket;
 
 
 public class GameController {
@@ -36,18 +38,46 @@ public class GameController {
     @FXML
     public ImageView deck;
 
-
-
-
     @FXML
     public void initialize() {
 
-        Card c = new Card(CardSuite.CLUBS, CardRank.KING);
-        hBox.getChildren().add(new ImageView(c.getImage()));
+        /*Card c = new Card(CardSuite.CLUBS, CardRank.KING);
+        hBox.getChildren().add(new ImageView(c.getImage()));*/
+
+        Card[] testHand = new Card[4];
+
+        testHand[0] = new Card(CardSuite.CLUBS, CardRank.KING);
+        testHand[1] = new Card(CardSuite.DIAMONDS, CardRank.KING);
+        testHand[2] = new Card(CardSuite.HEARTS, CardRank.KING);
+        testHand[3] = new Card(CardSuite.SPADES, CardRank.KING);
+
+        setHand(testHand);
 
         Card ca = new Card(CardSuite.SPADES, CardRank.QUEEN);
         currentCard.setImage(ca.getImage());
 
+    }
+
+    /**
+     * Sets the view according to a packet sent from the server
+     * @param packet A packet describing the game state
+     */
+    public void setGameState(DataPacket packet) {
+
+    }
+
+    /**
+     * Sets the player's displayed hand.
+     * @param playerHand Array of cards
+     */
+    public void setHand(Card[] playerHand) {
+        ObservableList<Node> children = hBox.getChildren();
+
+        children.clear();
+
+        for (Card c : playerHand) {
+            children.add(new ImageView(c.getImage()));
+        }
     }
 
     // Buttonactions for the whish cardSuite buttons
