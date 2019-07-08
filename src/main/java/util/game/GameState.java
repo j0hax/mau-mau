11cc1d93fa5e6b2df505b29c6ef9be4edd4877e8
johannes.arnold <johannes.arnold@stud.uni-hannover.de>
2019-07-08 17:45:08 +0200

    Updated GameState

--- src/main/java/util/game/GameState.java
+++ src/main/java/util/game/GameState.java
@@ -5,12 +5,18 @@ import util.cards.Card;
 public class GameState {
 
     private Card[] hand = new Card[32];
-    private String playerName = "";
+    private String activePlayer = "";
 
-    public GameState(String playerName, Card[] hand) {
-        this.playerName = playerName;
+    public GameState(String activePlayer, Card[] hand) {
+        this.activePlayer = activePlayer;
         this.hand = hand;
     }
 
+    public Card[] getHand() {
+        return hand;
+    }
 
+    public String getActivePlayer() {
+        return activePlayer;
+    }
 }
