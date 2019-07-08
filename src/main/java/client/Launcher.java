75f489e73877ef7e62b18d5622263cc6e4aead8d
johannes.arnold <johannes.arnold@stud.uni-hannover.de>
2019-07-08 17:25:50 +0200

    Debug message

--- src/main/java/client/Launcher.java
+++ src/main/java/client/Launcher.java
@@ -2,6 +2,7 @@ package client;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println("Launching JavaFX Client...");
         StartClient.main(args);
     }
 }
