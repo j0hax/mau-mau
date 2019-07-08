258c57c0e4f3be2751c2f6e181b1b7b29a4ac2f9
johannes.arnold <johannes.arnold@stud.uni-hannover.de>
2019-07-08 17:52:49 +0200

    Switched to use an index of the player which has a turn Added documentation

--- src/main/java/util/game/GameState.java
+++ src/main/java/util/game/GameState.java
@@ -2,21 +2,34 @@ package util.game;
 
 import util.cards.Card;
 
+/**
+ * Represents the current state of the game for an individual player
+ */
 public class GameState {
 
     private Card[] hand = new Card[32];
-    private String activePlayer = "";
+    private int activePlayerIndex = 0;
 
-    public GameState(String activePlayer, Card[] hand) {
-        this.activePlayer = activePlayer;
+    /**
+     * @param activePlayer the index of the currently active player (from String[] allPlayers in class NewGame)
+     * @param hand         the individual players current hand
+     */
+    public GameState(int activePlayer, Card[] hand) {
+        this.activePlayerIndex = activePlayerIndex;
         this.hand = hand;
     }
 
+    /**
+     * @return The hand of the current player
+     */
     public Card[] getHand() {
         return hand;
     }
 
-    public String getActivePlayer() {
-        return activePlayer;
+    /**
+     * @return The index of the player who has the turn
+     */
+    public int activePlayerIndex() {
+        return activePlayerIndex;
     }
 }
